95974e0a985bacad2b19c515a42693fcc1114690
JavierCarrillo4 <javiercarrillo424@gmail.com>
2024-09-06 02:38:56 -0400

    Add files via upload

---
+++ Events.lua
@@ -0,0 +1,6 @@
+game.Players.PlayerAdded:Connect(function(player)	-- when players join the game immediately
+    -- connect the players this function
+print("A player has joined the game!")
+end)
+
+-- Google search "Roblox Events List" for a list of all events within roblox's engine
